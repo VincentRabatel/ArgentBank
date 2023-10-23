f4622cec522c3a09323d923832650a924d9221e6
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-10-23 08:03:01 +0300

    Create User page and Account component

---
+++ client/src/components/Account/Account.js
@@ -0,0 +1,21 @@
+import './Account.css';
+
+function Account({title, amount, description}) {
+
+    return (
+        <section class="account">
+            <div className="account-content-wrapper">
+                <h3 className="account-title">{title}</h3>
+                <p className="account-amount">{amount}</p>
+                <p className="account-amount-description">{description}</p>
+            </div>
+
+            {/* todo: create a component for this button */}
+            <div className="account-content-wrapper cta">
+                <button className="transaction-button">View transactions</button>
+            </div>
+        </section>
+    )
+}
+
+export default Account;
--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -4,12 +4,59 @@ import './User.css';
 
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer.js/Footer';
+import Account from '../../components/Account/Account';
+
+// Create objects to configure Features
+const accountA = {
+    title: "Argent Bank Checking (x8349)",
+    amount: "$2,082.79", 
+    description: "Available Balance"
+}
+
+const accountB = {
+    title: "Argent Bank Savings (x6712)",
+    amount: "$10,928.42", 
+    description: "Available Balance"
+}
+
+const accountC = {
+    title: "Argent Bank Credit Card (x8349)",
+    amount: "$184.30", 
+    description: "Current Balance"
+}
 
 function User() {
     return (
         <Fragment>
             <Header />
-            <h1>User</h1>
+
+            <main className="main bg-dark">
+                <div className="user-header">
+                    <h1>Welcome back<br />Tony Jarvis!</h1>
+
+                    {/* todo: create a component for this button */}
+                    <button className="edit-button">Edit Name</button>
+                </div>
+
+                <h2 className="sr-only">Accounts</h2>
+
+                <Account 
+                    title={accountA.title} 
+                    amount={accountA.amount} 
+                    description={accountA.description} 
+                />
+                <Account 
+                    title={accountB.title} 
+                    amount={accountB.amount} 
+                    description={accountB.description} 
+                />
+                <Account 
+                    title={accountC.title} 
+                    amount={accountC.amount} 
+                    description={accountC.description} 
+                />
+            </main>
+                
             <Footer />
         </Fragment>
     )
